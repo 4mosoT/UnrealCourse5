30c8fdc237f87d65a6f0376536d1e12fc22ed0fe
Marco <marco.a.formoso@gmail.com>
2017-11-28 21:41:29 +0100

    ChooseNextWaypoint Added

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,10 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
 	Super::ExecuteTask(OwnerComp, NodeMemory);
-	UE_LOG(LogTemp, Warning, TEXT("NodeTaskWorking"))
+	UBlackboardComponent* BlackBoardComp = OwnerComp.GetBlackboardComponent();
 	return EBTNodeResult::Succeeded;
 }
