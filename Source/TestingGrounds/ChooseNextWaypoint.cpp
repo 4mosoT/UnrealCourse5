a00a87340af22cf901368a3d40d07eaacf27f562
Marco <marco.a.formoso@gmail.com>
2017-12-04 16:25:43 +0100

    Added Patrolling Component

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -4,17 +4,26 @@
 #include "BehaviorTree/BehaviorTreeComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+#include "PatrollingComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
 	Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	UPatrollingComponent* Component = OwnerComp.GetAIOwner()->GetPawn()->FindComponentByClass<UPatrollingComponent>();
+	if (!ensure(Component)) return EBTNodeResult::Failed;
+
+	TArray<AActor*> PatrolPoints = Component->GetPatrolPoints();
+	if(PatrolPoints.Num() == 0){ 
+		UE_LOG(LogTemp, Warning, TEXT("No patrol points"))
+		return EBTNodeResult::Failed;
+	}
+
 	UBlackboardComponent* BlackBoardComp = OwnerComp.GetBlackboardComponent();
 	int32 Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	APatrollingGuard* Guard = Cast<APatrollingGuard>(OwnerComp.GetAIOwner()->GetPawn());
-	TArray<AActor*> PatrolPoints = Guard->GetPatrolPoints();
 	BlackBoardComp->SetValueAsObject(WayPointKey.SelectedKeyName, PatrolPoints[Index]);
 	BlackBoardComp->SetValueAsInt(IndexKey.SelectedKeyName, (Index + 1) % PatrolPoints.Num());
+	
 	return EBTNodeResult::Succeeded;
 
 }
