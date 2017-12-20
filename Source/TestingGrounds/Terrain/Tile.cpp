0f24fb2a842b8b911dde0586b820906462f562e6
Marco <marco.a.formoso@gmail.com>
2017-12-20 21:45:20 +0100

    Draw Debug Sphere

--- Source/TestingGrounds/Terrain/Tile.cpp
+++ Source/TestingGrounds/Terrain/Tile.cpp
@@ -2,6 +2,7 @@
 
 #include "Tile.h"
 #include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 
 
 // Sets default values
@@ -30,13 +31,29 @@ void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int32 MinSpawn, int32 MaxSp
 void ATile::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	if(CastSphere(GetActorLocation(), 300))	DrawDebugSphere(GetWorld(), GetActorLocation(), 300, 32, FColor::Red, true, 1000); 
+	else DrawDebugSphere(GetWorld(), GetActorLocation(), 300, 32, FColor::Green, true, 1000);
+
 }
 
 // Called every frame
 void ATile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+}
+
+bool ATile::CastSphere(FVector Location, float Radius)
+{
+	FHitResult HitResult;
+	return GetWorld()->SweepSingleByChannel(
+		HitResult,
+		Location,
+		Location,
+		FQuat::Identity,
+		ECollisionChannel::ECC_Camera,
+		FCollisionShape::MakeSphere(Radius)
+	);
 
 }
 
